83afb540298796ccf2e930b5ba5966c1f0a2022a
NINANGE <m13672456277@163.com>
2017-09-18 17:15:11 +0800

    commentSpider sec commit

--- startSpider/startSpiderTask.sh
+++ startSpider/startSpiderTask.sh
@@ -7,5 +7,5 @@ cd /home/django/nange/commentSpider/commentPrint
 currentTime=$(date -d 'now' +%Y%m%d)
 
 #执行.py中定义的项目example，并指定日志文件，其中nohup....&表示可以在后台执行，不会因为关闭终端而导致程序执行中断。
-nohup python comSpider.py >> "/home/django/nange/taoBaoSpider/taoBaoScrapy/errorDic/${currentTime}taoBaoSpider.log" 2>&1 &
+nohup python comSpider.py >> "/home/django/nange/commentSpider/commentPrint/${currentTime}commentPrints.log" 2>&1 &
 #nohup scrapy crawl taoBaoSpider >> "/home/django/nange/taoBaoSpider/taoBaoScrapy/errorDic/${currentTime}taoBaoSpider.log" 2>&1 &
